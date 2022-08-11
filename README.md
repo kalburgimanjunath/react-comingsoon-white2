0fb16a5cdb9d062293bb182fad3674ca0d9eabbb
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-11 16:17:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bkbwbs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bkbwbs)
---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './style.css';
+import Timer from './Timer';
+export default function App() {
+  const Ref = useRef(null);
+  const [timer, setTimer] = useState('00:00:00');
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      // update the timer
+      // check if less than 10 then we need to
+      // add '0' at the beginning of the variable
+      setTimer(
+        (hours > 9 ? hours : '0' + hours) +
+          ':' +
+          (minutes > 9 ? minutes : '0' + minutes) +
+          ':' +
+          (seconds > 9 ? seconds : '0' + seconds)
+      );
+    }
+  };
+
+  const clearTimer = (e) => {
+    // If you adjust it you should also need to
+    // adjust the Endtime formula we are about
+    // to code next
+    setTimer('00:00:10');
+
+    // If you try to remove this line the
+    // updating of timer Variable will be
+    // after 1000ms or 1sec
+    if (Ref.current) clearInterval(Ref.current);
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000);
+    Ref.current = id;
+  };
+
+  const getDeadTime = () => {
+    let deadline = new Date();
+
+    // This is where you need to adjust if
+    // you entend to add more time
+    deadline.setSeconds(deadline.getSeconds() + 10000);
+    return deadline;
+  };
+
+  // We can use useEffect so that when the component
+  // mount the timer will start as soon as possible
+
+  // We put empty array to act as componentDid
+  // mount only
+  useEffect(() => {
+    clearTimer(getDeadTime());
+  }, []);
+
+  // Another way to call the clearTimer() to start
+  // the countdown is via action event from the
+  // button first we create function to be called
+  // by the button
+  const onClickReset = () => {
+    clearTimer(getDeadTime());
+  };
+  // useEffect(() => {
+  //   // console.log(getTimer());
+  //   const time = new Date();
+  //   setTimeout(setTimer(getTimer(time)), 1000);
+  //   // setInterval(timer, 1000);
+  // });
+  return (
+    <div className="coming">
+      <div className="container">
+        <h1>Coming Soon</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          tristique, magna ut tincidunt lacinia, ante erat hendrerit est, id
+          varius tortor ipsum.
+        </p>
+        <h2>{timer}</h2>
+        {/* <button onClick={onClickReset}>Reset</button> */}
+        <div className="form">
+          <div>
+            <input type="text" name="email" placeholder="Email address" />
+          </div>
+
+          <div>
+            <button type="submit">Notify Me</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
